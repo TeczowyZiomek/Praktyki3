315803f66297048a7053b6c71c6479d289a4a2ed
Szymon Teskowski <teskowski.szymon@gmail.com>
2025-11-21 10:05:41 +0100

    pierwszaInicjacja v4

--- czas test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "Dziś jest: " .date("d-m-Y"). "<br>";
-echo "Teraz jest: " .date("H:i:s"). "<br>";
-date_default_timezone_set("America/New_York");
-echo "A w Ameryce: " .date("H:i:s");
-
-?>
